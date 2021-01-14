717ba2264b882f5b6282c5b9d96ff1640eec24db
Vashisth00 <vashisthbhushan@gmail.com>
2021-01-14 16:58:37 +0530

    debug

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import HomePage from './pages/HomePage';
 
 import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
 
+const debug =
+  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
 
 // 1. Create a client engine instance
 const engine = new Styletron();
@@ -13,6 +15,11 @@ const engine = new Styletron();
 function App() {
   return (
     <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+      <Router>
+        <Route>
+
+        </Route>
+      </Router>
     <App />
   </StyletronProvider>
   );
